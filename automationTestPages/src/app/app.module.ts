6260e0a06e7e72fdbf4b90cc388194d84e1fc407
inv-abinjose <abin.jose@innovaturelabs.com>
2021-04-20 12:11:44 +0530

    new components

--- automationTestPages/src/app/app.module.ts
+++ automationTestPages/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdddataComponent } from './adddata/adddata.component';
+import { ListdataComponent } from './listdata/listdata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdddataComponent,
+    ListdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
