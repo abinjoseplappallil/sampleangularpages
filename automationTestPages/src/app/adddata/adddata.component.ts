6d180cad249e99f7a3e634468d08400ed94fa434
amsmn <amalmn2718@gmail.com>
2021-04-20 13:10:41 +0530

    Update adddata.component.ts

--- automationTestPages/src/app/adddata/adddata.component.ts
+++ automationTestPages/src/app/adddata/adddata.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-adddata',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adddata.component.css']
 })
 export class AdddataComponent implements OnInit {
-
+  form = new FormGroup({
+    gender: new FormControl('', Validators.required),
+    name:  new FormControl('', Validators.required),
+    dob:new FormControl('', Validators.required),
+  });
+   
+  get f(){
+    return this.form.controls;
+  }
+   
+  submit(){
+    console.log(this.form.value);
+  }
+ 
+  changeGender(e) {
+    console.log(e.target.value);
+  }
   constructor() { }
 
   ngOnInit(): void {
