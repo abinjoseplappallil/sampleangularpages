79ec78402ab10cd2a61473d1d5263b2818d1b13d
inv-abinjose <abin.jose@innovaturelabs.com>
2021-04-20 14:55:30 +0530

    list edit

--- automationTestPages/src/app/app.module.ts
+++ automationTestPages/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdddataComponent } from './adddata/adddata.component';
 import { ListdataComponent } from './listdata/listdata.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { ListdataComponent } from './listdata/listdata.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CommonModule, FormsModule, NgxDatatableModule, HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
