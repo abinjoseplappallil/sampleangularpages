b7e56019ce3670075b815e1cfa8f9a8648f516ce
amsmn <amalmn2718@gmail.com>
2021-04-20 13:10:47 +0530

    Update app.module.ts

--- automationTestPages/src/app/app.module.ts
+++ automationTestPages/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +16,9 @@ import { ListdataComponent } from './listdata/listdata.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
