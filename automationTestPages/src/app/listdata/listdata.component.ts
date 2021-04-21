10fa61603caf90ee026647fede02b8c93b2b3ae9
inv-abinjose <abin.jose@innovaturelabs.com>
2021-04-21 09:01:51 +0530

    removed unwanted

--- automationTestPages/src/app/listdata/listdata.component.ts
+++ automationTestPages/src/app/listdata/listdata.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit,ViewChild, AfterViewInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { fromEvent } from 'rxjs';
-import { map, debounceTime } from 'rxjs/operators';
-import { FormGroup, FormControl, Validators,FormArray } from '@angular/forms';
+
+import { FormGroup, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-listdata',
   templateUrl: './listdata.component.html',
   styleUrls: ['./listdata.component.css']
 })
-export class ListdataComponent implements OnInit , AfterViewInit {
+export class ListdataComponent implements OnInit  {
   @ViewChild('search', { static: false }) search: any;
   
   countries: any = ['India', 'britan', 'japan', 'india','russia',  'USA'];
@@ -26,64 +24,10 @@ export class ListdataComponent implements OnInit , AfterViewInit {
   public nameselected:string=" ";
   public countryelected:string="choose country";
   public genderelected:string="choose gender";
-  constructor(private httpClient: HttpClient) { }
-  ngAfterViewInit(): void {
-  
-    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    // Add 'implements AfterViewInit' to the class.
-    fromEvent(this.search.nativeElement, 'keydown')
-      .pipe(
-        debounceTime(550),
-        map(x => x['target']['value'])
-      )
-      .subscribe(value => {
-        this.updateFilter(value);
-      });
-  }
-  
-  updateFilter(val: any) {
-    const value = val.toString().toLowerCase().trim();
-    // get the amount of columns in the table
-    const count = this.columns.length;
-    
-    // get the key names of each column in the dataset
-    const keys = Object.keys(this.temp[0]);
-    // assign filtered matches to the active datatable
-    this.rows = this.temp.filter(item => {
-  
-      if (
-        (item[keys[0]] &&
-          item[keys[0]]
-            .toString()
-            .toLowerCase()
-            .indexOf(value) == 0) ||
-        !value
-      ) {
-        // found match, return true to add to result set
-        return true;
-      }
-      // iterate through each row's column data
-      
-    });
+  constructor() { }
 
-    // Whenever the filter changes, always go back to the first page
-    // this.table.offset = 0;
-  }
 
-  findAll() {
-   this.httpClient.get('./assets/data/employee.json').subscribe(
-      (data: any) => {
-        // cache our list
-        this.temp = data;
-
-        // push our inital complete list
-        this.rows = [...this.temp];
-      },
-      (err: HttpErrorResponse) => {
-    
-      }
-    );
-  }
+  
 
   getDataJson() {
     this.rows = this.temp = [
